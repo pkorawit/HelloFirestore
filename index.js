326f4e2cb4b2a07e3b4cae0f410c47ef17609649
Korawit Prutsachainimmit <korawit@Korawits-MacBook-Pro.local>
2017-10-11 16:38:30 +0700

    Demo for Firebase database

---
+++ index.js
@@ -0,0 +1,60 @@
+$(function () {
+
+    // const firebase = require("firebase");
+    // // Required for side-effects
+    // require("firebase/firestore");
+
+    firebase.initializeApp({
+        apiKey: 'AIzaSyBJKQnNW-VN7RJlBtp_kQrMsZS6ElNtdxk',
+        authDomain: 'demowebclient.firebaseapp.com',
+        projectId: 'demowebclient'
+    });
+
+    // Initialize Cloud Firestore through Firebase
+    var db = firebase.firestore();
+
+    $('#save').click(function () {
+
+        var username = $('#username').val();
+        var fullname = $('#fullname').val();
+        var mobileno = $('#mobileno').val();
+
+        db.collection("users").add({
+            username: username,
+            fullname: fullname,
+            mobileno: mobileno
+        })
+            .then(function (docRef) {
+                console.log("Document written with ID: ", docRef.id);                
+
+                $('#tablebody').empty();
+
+                db.collection("users").get().then(function(querySnapshot) {
+                    querySnapshot.forEach(function(doc) {
+
+                        console.log(doc.id, " => ", doc.data());
+                        var username = doc.username;
+                        var fullname = doc.fullname;
+                        var mobileno = doc.mobileno;
+
+                        var row = "<tr>" +
+                        "<th scope='row'>" + doc.id + "</th>" +
+                        "<td>" + doc.data().username + "</td>" +
+                        "<td>" + doc.data().fullname + "</td>" +
+                        "<td>" + doc.data().mobileno + "</td>" +
+                        "</tr>"
+
+                        $('#tablebody').append(row);
+
+                    });
+                });
+            
+            })
+            .catch(function (error) {
+                console.error("Error adding document: ", error);
+            });
+
+    });
+
+
+})
