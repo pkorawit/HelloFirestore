fc3dabf33768adf8c435402d39efb0aa2abc77e0
Korawit Prutsachainimmit <korawit@Korawits-MacBook-Pro.local>
2017-10-17 18:48:09 +0700

    Added Authen

---
+++ login.js
@@ -0,0 +1,32 @@
+$(function () {
+
+    $("#authen").click(function () {
+        var email = $("#email").val();
+        var password = $("#password").val();
+        firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorCode);
+            console.log(errorMessage);
+        });
+
+        firebase.auth().onAuthStateChanged(function (user) {
+            if (user) {
+                // User is signed in.
+                var displayName = user.displayName;
+                var email = user.email;
+                var emailVerified = user.emailVerified;
+                var photoURL = user.photoURL;
+                var isAnonymous = user.isAnonymous;
+                var uid = user.uid;
+                var providerData = user.providerData;
+                console.log(email + " is signed in");
+            } else {
+                console.log(email + " is signed out");
+            }
+        });
+    });
+
+
+});
